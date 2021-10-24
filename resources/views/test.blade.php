bd08436e260cbbe24e6f58e7c07a3732e9aef41a
Rami Kellesly <rami.kelesli@gmail.com>
2021-10-24 12:01:24 +0300

    Hijri api 24102021

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,27 +1,15 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
+<h1>Hijri test </h1>
+https://github.com/alkoumi/laravel-hijri-date <br>
+composer require alkoumi/laravel-hijri-date <br>
+in rotes/web.php  <br>
+------------------
 use Alkoumi\LaravelHijriDate\Hijri;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/test', function () {
     return view('test');
 });
 Route::get('/getgreg/{dt}', function ($dt) {
-    //http://127.0.0.1:8000/getgreg/25-02-1440
+    //http://127.0.0.1:8002/getgreg/25-02-1440
     $day=sprintf("%02d", substr($dt,0,2));
     $month=sprintf("%02d", substr($dt,3,2));
     $year=substr($dt,6,4);
