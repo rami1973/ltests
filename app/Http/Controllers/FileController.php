c77cc0962be3919ab9301d2050fd79fa625ade39
rami1973 <rami.kelesli@gmail.com>
2023-08-14 11:51:39 +0300

    rmk14082023

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -19,4 +19,13 @@ $file = $disk->temporaryUrl('images/'.$filename, now()->addMinutes(1));
 return (new Response('',301,['Location' => $file]));
 
      }
+    public function getFile2($fpath,$filename)
+	{
+        $fpath=str_replace(',','/',$fpath);
+        $disk = Storage::disk('oci');
+
+$file = $disk->temporaryUrl($fpath.'/'.$filename, now()->addMinutes(1));
+return (new Response('',301,['Location' => $file]));
+
+     }
 }
---
+++ resources/views/graph.blade.php
@@ -0,0 +1,232 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+ 
+
+    <img src="http://127.0.0.1:1444/images/logo.png" width="244" height="134" border="0" align="left">
+
+        <p style="text-align: right;font-size: 15px;font-weight: bold;letter-spacing: 1px;">
+
+    &nbsp;	Sraco Company - شـركـة سـراكـو<br>
+
+         قسم الحركه والسيارات</p>
+
+         <div style="text-align: center;font-size: 15px;font-weight: bold;letter-spacing: 1px;">نموذج تصفية حادث 2022</div>
+
+         <p style="text-align: right;font-size: 15px;font-weight: bold;letter-spacing: 1px;">
+
+         من / قسم الحركة والسيارات<br>
+
+         إلى/ الإدارة المالية<br>
+
+        مُرفق إلى حضراتكم نموذج تصفية حادث سياره موضح بها بيانات السياره وتفاصيل الحادث وقيمة التعويض<br>
+
+         </p>
+
+         <div style="">
+
+        <table dir="ltr" style="text-align: center;font-size: 15px;width:100%;" height="100">
+
+        <tbody><tr><td colspan="6" height="40px">بيانات السيارات</td></tr>
+
+        <tr>
+
+        <td width="40%" height="40px">إسم المشروع
+
+        </td><th width="20%" height="40px">نوع السياره</th>
+
+        <th width="10%" height="40px">الموديل</th>
+
+        <td width="15%" height="40px">رقم اللوحه
+
+        </td><td width="15%" height="40px">رقم سراكو
+
+        </td></tr>
+
+    <tr>
+
+        <td width="40%" height="40px">خساره كُليه</td>
+
+        <td width="20%" height="40px">N/A</td>
+
+        <td width="10%" height="40px">2010</td>
+
+        <td width="15%" height="40px">أ ق ب 2461</td>
+
+        <td width="15%" height="40px">1122</td>
+
+        </tr>
+
+        <tr>
+
+    </tr><tr>
+
+
+
+        </tr>
+
+    </tbody></table>
+
+
+
+         </div>
+
+
+
+         <div style="">
+
+        <table dir="ltr" style="text-align: center;font-size: 15px;width:100%;" height="100px">
+
+        <tbody><tr><td colspan="8" height="40px">تفاصيل الحادث</td></tr>
+
+        <tr>
+
+        <td colspan="2" width="20%" height="40px">رقم المُطالبه
+
+        </td><th width="10%" height="40px">تكلفة الإصلاح</th>
+
+        <th width="10%" height="40px">غ نسبة الخطأ</th>
+
+        <th width="10%" height="40px">نسبة الخطأ</th>
+
+        <th width="30%" height="40px">الإسم</th>
+
+        <td width="10%" height="40px">رقم الملف
+
+        </td><th width="10%" height="40px">تاريخ الحادث</th>
+
+        </tr>
+
+    <tr>
+
+        <td colspan="2" width="20%" height="40px">c/410/6501/13/026892</td>
+
+        <td width="10%" height="40px">40890</td>
+
+        <td class="accident_driver_amount" width="10%" height="40px"></td>
+
+        <td id="accident_driver_per_s" width="10%" height="40px">
+            <input type="hidden" value="$accident_per" id="accident_per">
+            <input type="hidden" value="0" id="accident_driver_per">
+
+        </td>
+
+        <td width="30%" height="40px">SAMSER ABDUL  JABBAR</td>
+
+        <td width="10%" height="40px"> 34490          </td>
+
+        <td width="10%" height="40px">2013-11-21</td>
+
+        </tr>
+
+        <tr>
+
+        <td colspan="2" width="20px" height="40px">الإجمالي
+
+        </td><th colspan="2" width="400px" height="40px">قيمة التحمل</th>
+
+        <td colspan="4" rowspan="2">خسارة كلية
+  
+
+  
+
+  
+
+  
+
+  </td>
+
+        </tr>
+
+        <tr>
+
+        <td colspan="2" class="accident_driver_amount" width="20px" height="40px"></td>
+
+        <td colspan="2" width="3%" height="40px">0</td>
+
+        </tr>
+
+        <tr>
+
+    </tr><tr>
+
+
+
+        </tr>
+
+    </tbody></table>
+
+    </div>
+
+    <div style="">
+
+             <table dir="ltr" style="text-align: center;font-size: 15px;width:100%;" height="100">
+
+        <tbody><tr><td colspan="6" height="40px">تفاصيل التعويض</td></tr>
+
+        <tr>
+
+        <td colspan="2" width="20px" height="40px">صافي التعويض
+
+        </td><th width="400px" height="40px">قيمة الإستهلاك</th>
+
+        <th width="400px" height="40px">قيمة المطالبه</th>
+
+        </tr>
+
+            <tr>
+
+        <td colspan="2" id="net_amount" width="20px" height="40px"></td>
+
+        <td width="3%" height="40px">4500</td>
+
+        <td width="3%" height="40px">45000</td>
+
+        </tr>
+
+        </tbody></table>
+
+         </div>
+
+         <div style="">
+
+         <table dir="ltr" style="text-align: center;font-size: 15px;width:100%;" height="100">
+
+        <tbody><tr>
+
+        <td colspan="6" height="40px">التوقيع</td></tr>
+<tr>
+        <th width="400px" height="40px">مدير الخدمات المساندة</th>
+
+        <th width="400px" height="40px">مدير قسم الحركة</th>
+
+    <th width="400px" height="40px">مسؤول التأمين و الحودث</th>
+
+
+
+    </tr><tr>
+
+        <td width="3%" height="50px"></td>
+
+        <td width="3%" height="50px"></td>
+
+        <td width="3%" height="50px"></td>
+
+        </tr>
+
+        </tbody></table>
+
+         </div>
+
+
+
+    
+</body>
+</html>
